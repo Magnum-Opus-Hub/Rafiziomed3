b664fcfc21a126552fae1bb571864797ce6cbd84
FilipTheGod <filipstan666@gmail.com>
2023-06-05 23:42:53 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const colors = require("colors");
+const moragan = require("morgan");
+const dotenv = require("dotenv");
+const userRoutes = require('./routes/userRoute');
+const adminRoutes = require("./routes/adminRoute");
+const doctorRoutes = require("./routes/doctorRoute")
+const connectDb = require("./config/connectDb");
+const path = require("path");
+const { default: mongoose } = require("mongoose");
+
+//dotenv conig
+dotenv.config();
+connectDb();
+
+//rest obejct
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(moragan("dev"));
+
+//routes
+app.use("/api/user", userRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/doctor", doctorRoutes);
+
+//static files
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+//port
+const PORT = process.env.PORT || 4001;
+//listen port
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection established successfully")
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`.bgCyan.white);
+  });
+
+});
